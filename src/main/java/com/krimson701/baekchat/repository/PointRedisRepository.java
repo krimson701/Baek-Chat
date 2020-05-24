0940e270005fcee0231c8d960afb2a67598229bb
do168 <54763136+do168@users.noreply.github.com>
2020-05-24 13:24:41 +0900

    Delete PointRedisRepository.java

--- src/main/java/com/krimson701/baekchat/repository/PointRedisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.krimson701.baekchat.repository;
-
-import com.krimson701.baekchat.domain.Point;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PointRedisRepository extends CrudRepository<Point, String> {
-}
