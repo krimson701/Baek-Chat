a455c812b84248b0078df79f4d0d643bf0778313
krimson701 <krimson701@gmail.com>
2020-05-30 13:15:05 +0900

    로그인 관련 리턴값

--- src/main/java/com/krimson701/baekchat/configuration/common/interceptor/AuthCheckInterceptor.java
+++ src/main/java/com/krimson701/baekchat/configuration/common/interceptor/AuthCheckInterceptor.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -37,18 +38,22 @@ public class AuthCheckInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object controller) {
         //log.info(request.getRequestURI());
         log.info(">> AuthCheckInterceptor preHandle Start<<");
-
+        log.info("URI : [{}]", request.getRequestURI());
         Enumeration<String> it = request.getHeaderNames();
         while(it.hasMoreElements()) {
             String temp = it.nextElement();
             log.info("[{}] : [{}]", temp, request.getHeader(temp));
         }
         log.info("token : [{}]", request.getHeader("authorization"));
-        String googleUrl = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=" + request.getHeader("authorization");
-        ResponseEntity<AuthGoogleInfo> googleResponse = restTemplate.exchange(googleUrl, HttpMethod.GET, null, AuthGoogleInfo.class);
+        ResponseEntity<AuthGoogleInfo> googleResponse = null;
+        try {
+            String googleUrl = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=" + request.getHeader("authorization");
+            googleResponse = restTemplate.exchange(googleUrl, HttpMethod.GET, null, AuthGoogleInfo.class);
+        } catch (HttpClientErrorException e){
+            log.debug("Invalid Google Token");
+            return false;
+        }
         AuthGoogleInfo authData = googleResponse.getBody();
-        log.info("response: [{}]", googleResponse.getBody());
-
         Long userId = Long.valueOf(authData.getUserId().substring(2));// String to Long
         log.info("userId cut : [{}]", userId);
 
--- src/main/java/com/krimson701/baekchat/controller/LoginController.java
+++ src/main/java/com/krimson701/baekchat/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.krimson701.baekchat.controller;
 
+import com.krimson701.baekchat.domain.User;
 import com.krimson701.baekchat.model.UserDto;
 import com.krimson701.baekchat.repository.UserRepository;
 import com.krimson701.baekchat.service.UserService;
@@ -15,11 +16,9 @@ import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 import springfox.documentation.annotations.ApiIgnore;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @Slf4j
 @Api(value="Oauth-controller", description="Oauth controller")
@@ -48,20 +47,18 @@ public class LoginController {
             @ApiResponse(code=200, message="success"),
     })
     @RequestMapping(value="/signIn", method= RequestMethod.POST)
-    public ResponseEntity<Void> signIn(
+    public ResponseEntity<User> signIn(
             @ApiIgnore @RequestAttribute("userId") Long userId
             , @ApiIgnore @RequestAttribute("userEmail") String userEmail) {
         // 넘어오는 User 데이터가 많아지면 id와 email만 받는게아니라 userAuthModel형식으로 모델로 받을것
         // 그러려면 당연히 인터셉터에서 모델로 보내야한다.
-        if(!userRepository.findById(userId).isPresent()) {
+        Optional<User> userInfo = userRepository.findById(userId);
+        if(!userInfo.isPresent()) {
             log.info("newUser insert Execute");
             UserDto newUser = new UserDto(userId, userEmail);
             userService.insertUser(newUser);
         }
         log.info("userId : [{}]  Email : [{}]", userId, userEmail);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        return new ResponseEntity<>(userInfo.get(),HttpStatus.OK);
     }
 }
